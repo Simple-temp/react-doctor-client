152fb62f3c08bad90c8a92da3a0c37a67b8e2872
Simple-temp <fiveriam01@gmail..com>
2022-01-27 11:17:26 +0600

    dashboard complete

--- src/components/AllDashboard/AllDashboard/AllDashboard.js
+++ src/components/AllDashboard/AllDashboard/AllDashboard.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
+import DashboardItem from '../DashboardItem/DashboardItem';
 
 const AllDashboard = () => {
+
+    const [getdata, setGetdata] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/getallpatientsdata`)
+            .then(res => res.json())
+            .then(data => setGetdata(data))
+    }, [])
+
     return (
         <div className='container-fluid'>
             <div className="row">
                 <div className="col-lg-2">
                     <Sidebar></Sidebar>
                 </div>
-                <div className="col-lg-10">
-                    <h1>AllDashboard</h1>
+                <div className="col-lg-10 second">
+                    <h5 className='py-3 h5'>Dashboard</h5>
+                    <DashboardItem getdata={getdata}></DashboardItem>
                 </div>
             </div>
         </div>
---
+++ src/components/AllDashboard/DashboardItem/DashboardItem.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import "./DashboardItem.css"
+
+const DashboardItem = ({ getdata }) => {
+    return (
+        <div>
+            <div className="container pt-4 inner-width">
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                        <div className="d-box bg-danger p-3 m-2 d-flex">
+                            <span>09</span>
+                            <p>Pending <br /> Appointments</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                        <div className="d-box bg-primary p-3 m-2 d-flex">
+                            <span>19</span>
+                            <p>Today's <br /> Appointments</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                        <div className="d-box bg-success p-3 m-2 d-flex">
+                            <span>34</span>
+                            <p>Total <br /> Appointments</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                        <div className="d-box bg-warning p-3 m-2 d-flex">
+                            <span>78</span>
+                            <p>Total <br /> Patients</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container pt-4 mt-4">
+                <table className='table table-borderless'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Sl</th>
+                            <th scope='col'>Date</th>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>Contact</th>
+                            <th scope='col'>Prescription</th>
+                            <th scope='col'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            getdata.map((getdata, index) =>
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{getdata.createdDate}</td>
+                                    <td>{getdata.name}</td>
+                                    <td>{getdata.phone}</td>
+                                    <td><button className='btn btn-primary'>View</button></td>
+                                    <td>
+                                        <button className='btn btn-info mr text-white'>Pending</button>
+                                        <button className='btn btn-warning'><i class="fas fa-pencil-alt"></i></button>
+                                    </td>
+                                </tr>
+                            )
+
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardItem;
